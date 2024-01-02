c4dc037a3f4f361ba5382de9356f7cf1d98bb7e2
George <96517504+KacperLewa@users.noreply.github.com>
2024-01-02 19:06:28 +0100

    pomocy

--- Detektor_Altek/src/main/java/com/gitcompany/detektor_altek/MainFrame.java
+++ Detektor_Altek/src/main/java/com/gitcompany/detektor_altek/MainFrame.java
@@ -17,7 +17,7 @@ public class MainFrame extends javax.swing.JFrame {
         initComponents();
         Detector dt = new Detector();
     }
-
+    //bruh
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
